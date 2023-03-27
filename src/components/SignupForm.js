730850ccc28af7241622d4a1cbca704018347c6c
Shouvik Mukhopadhyay <mukhopadhyay.shouvik24@gmail.com>
2023-03-28 00:59:51 +0530

    Added all the Code Files and Assets Folders

---
+++ src/components/SignupForm.js
@@ -0,0 +1,193 @@
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+
+const SignupForm = ({ setIsLoggedIn }) => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    accountType: "",
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [accountType, setAccountType] = useState("student");
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  function changeHandler(event) {
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  function submiteHandler(event) {
+    event.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
+    setIsLoggedIn(true);
+    toast.success("Account Created");
+    const accountData = {
+      ...formData,
+    };
+
+    const finalData = {
+      ...accountData,
+      accountType,
+    };
+    console.log("Printing Final Account data : ");
+    console.log(finalData);
+
+    navigate("/dashboard");
+  }
+
+  return (
+    <div>
+      {/* Student-Instuctor Tab */}
+
+      <div className="flex bg-richblack-800 p-1 gap-x-1 my-6 rounded-full max-w-max">
+        <button
+          className={`${
+            accountType === "student"
+              ? "bg-richblack-900 text-richblack-5"
+              : "bg-transparent text-richblack-200"
+          } py-2 px-5 rounded-full transition-all duration-200 `}
+          onClick={() => setAccountType("student")}
+        >
+          Student
+        </button>
+
+        <button
+          onClick={() => setAccountType("instructor")}
+          className={`${
+            accountType === "instructor"
+              ? "bg-richblack-900 text-richblack-5"
+              : "bg-transparent text-richblack-200"
+          } py-2 px-5 rounded-full transition-all duration-200 `}
+        >
+          Instructor
+        </button>
+      </div>
+
+      <form onSubmit={submiteHandler}>
+        {/* first name and lst name div */}
+        <div className="flex gap-x-4 mt-[20px]">
+          <label className="w-full">
+            <p className="text-[0.875rem] text-richblack-5 mb-1 leading-[1.375rem]">
+              First Name<sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              type="text"
+              required
+              name="firstname"
+              onChange={changeHandler}
+              placeholder="Enter First Name"
+              value={formData.firstname}
+              className="bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]"
+            />
+          </label>
+
+          <label className="w-full">
+            <p className="text-[0.875rem] text-richblack-5 mb-1 leading-[1.375rem]">
+              Last Name<sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              type="text"
+              required
+              name="lastname"
+              onChange={changeHandler}
+              placeholder="Enter Last Name"
+              value={formData.lastName}
+              className="bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]"
+            />
+          </label>
+        </div>
+        {/* Email Address */}
+        <div className="mt-[20px]">
+          <label className="w-full mt-[20px]">
+            <p className="text-[0.875rem] text-richblack-5 mb-1 leading-[1.375rem]">
+              Email Address<sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              type="email"
+              required
+              name="email"
+              onChange={changeHandler}
+              placeholder="Enter email address"
+              value={formData.email}
+              className="bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]"
+            />
+          </label>
+        </div>
+
+        {/* createpassword and confirm password */}
+        <div className="w-full flex gap-x-4  mt-[20px]">
+          <label className="relative w-full">
+            <p className="text-[0.875rem] text-richblack-5 mb-1 leading-[1.375rem]">
+              Create Password<sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              type={showPassword ? "text" : "password"}
+              required
+              name="password"
+              onChange={changeHandler}
+              placeholder="Enter Password"
+              value={formData.password}
+              className="bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]"
+            />
+            <span
+              className="absolute right-3 top-[38px] cursor-pointer"
+              onClick={() => setShowPassword((prev) => !prev)}
+            >
+              {showPassword ? (
+                <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+              ) : (
+                <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+              )}
+            </span>
+          </label>
+
+          <label className="relative w-full">
+            <p className="text-[0.875rem] text-richblack-5 mb-1 leading-[1.375rem]">
+              Confirm Password<sup className="text-pink-200">*</sup>
+            </p>
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              required
+              name="confirmPassword"
+              onChange={changeHandler}
+              placeholder="Confirm Password"
+              value={formData.confirmPassword}
+              className="bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]"
+            />
+            <span
+              className="absolute right-3 top-[38px] cursor-pointer"
+              onClick={() => setShowConfirmPassword((prev) => !prev)}
+            >
+              {showConfirmPassword ? (
+                <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+              ) : (
+                <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+              )}
+            </span>
+          </label>
+        </div>
+
+        <button className=" w-full bg-yellow-50 rounded-[8px] font-medium text-richblack-900 px-[12px] py-[8px] mt-6">
+          Create Account
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,38 @@
+// import Template from "../components/Auth/Template";
+// import signupImg from "../assets/signup.png";
+
+// function Signup({ setIsLoggedIn }) {
+//   return (
+//     <Template
+//       title="Join the millions learning to code with StudyNotion for free"
+//       description1="Build skills for today, tomorrow, and beyond."
+//       description2="Education to future-proof your career."
+//       image={signupImg}
+//       formType="signup"
+//       setIsLoggedIn={setIsLoggedIn}
+//     />
+//   );
+// }
+
+// export default Signup;
+
+import React from "react";
+import Template from "../components/Template";
+import signupImg from "../assets/signup.png";
+
+const Signup = ({ setIsLoggedIn }) => {
+  return (
+    <div>
+      <Template
+        title="Join the millions learning to code with StudyNotion for free"
+        desc1="Build skills for today, tomorrow, and beyond."
+        desc2="Education to future-proof your career."
+        image={signupImg}
+        formtype="signup"
+        setIsLoggedIn={setIsLoggedIn}
+      />
+    </div>
+  );
+};
+
+export default Signup;
